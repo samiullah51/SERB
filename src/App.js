d0c8d97e6deca43dfd24261632a504d9a6bed6a8
samiullah51 <samii51728@gmail.com>
2022-10-18 21:20:11 +0500

    working on navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
+import AcUnitIcon from "@mui/icons-material/AcUnit";
+import Home from "./pages/Home/Home";
 function App() {
   return (
     <div className="App">
-      <h1>Let me sstart working on SERB </h1>
-      <img src="https://play-lh.googleusercontent.com/Q8klPWjtLQrBeeP2oDAtA0H0CrYZBpK8ckF3HnqDMT2L6GGdsUCjYc75mfRkoQyhrwfS" />
+      <Home />
     </div>
   );
 }
