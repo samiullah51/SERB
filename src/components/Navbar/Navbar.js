73d9d69f3d9c532e5c3a6be93f7f018a7bc931b8
samiullah51 <samii51728@gmail.com>
2022-10-31 20:37:14 +0500

    working on navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,7 +1,17 @@
 import React from "react";
-
+import "./Navbar.css";
 function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <div className="navbar">
+      <div className="navbar__left">
+        <p>Search</p>
+        <p>Search</p>
+      </div>
+      <div className="navbar__right">
+        <p>Right</p>
+      </div>
+    </div>
+  );
 }
 
 export default Navbar;
