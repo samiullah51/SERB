306a6540958bb159936d60bfb256199f53f46ad9
samiullah51 <samii51728@gmail.com>
2022-11-02 21:24:03 +0500

    working on products

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -3,12 +3,14 @@ import "./Home.css";
 import Navbar from "../../components/Navbar/Navbar";
 import SuggestCategories from "../../components/SuggestCategories/SuggestCategories";
 import RecentProducts from "../../components/RecentProducts/RecentProducts";
+import Footer from "../../components/Footer/Footer";
 function Home() {
   return (
     <>
       <Navbar />
       <SuggestCategories />
       <RecentProducts />
+      <Footer />
     </>
   );
 }
