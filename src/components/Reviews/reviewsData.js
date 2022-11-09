3098d418f9afb3eb22acff5e877ebd33f9781f52
samiullah51 <samii51728@gmail.com>
2022-11-09 18:34:22 +0500

    added some random data for review

--- src/components/Reviews/reviewsData.js
+++ src/components/Reviews/reviewsData.js
@@ -4,7 +4,7 @@ export const reviewsData = [
     profileImg:
       "https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg?w=2000",
     fullName: "Sami Samiullah",
-    stars: 4.5,
+    stars: 2.5,
     createdAt: "03-Feb-2022",
     desc: "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it.",
   },
