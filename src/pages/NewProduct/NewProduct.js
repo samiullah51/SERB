ccb8bdea511411bfc448ddf73794f2d126566572
samiullah51 <samii51728@gmail.com>
2022-11-24 12:20:00 +0500

    working on new product form

--- src/pages/NewProduct/NewProduct.js
+++ src/pages/NewProduct/NewProduct.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Navbar from "../../components/Navbar/Navbar";
-import SingleInput from "../../components/SingleInput/SingleInput";
 import "./NewProduct.css";
 function NewProduct() {
   return (
@@ -12,12 +11,7 @@ function NewProduct() {
       <h1>Add New Product</h1>
       <div className="new__product">
         {/* form */}
-        <div className="product__form">
-          {/* Single Input */}
-          <SingleInput type="select" label="Category" />
-          <SingleInput type="text" label="Title" />
-          <SingleInput type="text" label="Title" />
-        </div>
+        <div className="product__form">{/* Single Input */}</div>
       </div>
     </>
   );
