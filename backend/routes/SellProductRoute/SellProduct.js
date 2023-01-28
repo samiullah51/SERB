1a727d99005dd69d14db99261ebb0d6ed0c5ea93
samiullah51 <samii51728@gmail.com>
2023-01-28 23:08:15 +0500

    working on products api

--- backend/routes/SellProductRoute/SellProduct.js
+++ backend/routes/SellProductRoute/SellProduct.js
@@ -3,6 +3,7 @@ const router = require("express").Router();
 const {
   verifyTokenAndAuthorization,
   verifyToken,
+  verifyTokenAndAdmin,
 } = require("../../routes/UserRoute/verifyToken");
 // Add product
 router.post("/sell/add", verifyToken, async (req, res) => {
@@ -15,12 +16,34 @@ router.post("/sell/add", verifyToken, async (req, res) => {
   }
 });
 
-// Get all Products
+// Get all products
+
+router.get("/all", async (req, res) => {
+  try {
+    const allProducts = await Product.find().sort({ createdAt: -1 });
+    res.status(200).json(allProducts);
+  } catch (err) {
+    res.status(500).json(err.message);
+  }
+});
+// Get Recent Products
 router.get("/recentproducts", async (req, res) => {
   try {
     const recentProducts = await Product.find()
       .limit(12)
       .sort({ createdAt: -1 });
+    res.status(200).json(recentProducts);
+  } catch (err) {
+    res.status(500).json(err.message);
+  }
+});
+
+// Get all products of a specific user
+router.get("/all/:userId", async (req, res) => {
+  try {
+    const recentProducts = await Product.find({
+      userId: req.params.userId,
+    }).sort({ createdAt: -1 });
     res.status(200).json(recentProducts);
   } catch (err) {
     res.status(500).json(err.message);
@@ -39,6 +62,32 @@ router.get("/relatedproducts", async (req, res) => {
       .limit(5)
       .sort({ createdAt: -1 });
     res.status(200).json(relatedProducts);
+  } catch (err) {
+    res.status(500).json(err.message);
+  }
+});
+
+// Update a product
+router.put("/edit/:productId", verifyToken, async (req, res) => {
+  try {
+    const updatedProduct = await Product.findByIdAndUpdate(
+      req.params.productId,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.status(201).json(updatedProduct);
+  } catch (err) {
+    res.status(500).json(err.message);
+  }
+});
+
+// Delete a product
+router.delete("/delete/:productId", verifyToken, async (req, res) => {
+  try {
+    const deleteProduct = await Product.findByIdAndDelete(req.params.productId);
+    res.status(201).json({ message: "Product deleted successfully" });
   } catch (err) {
     res.status(500).json(err.message);
   }
