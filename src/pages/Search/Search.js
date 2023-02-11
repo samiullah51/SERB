879776cd3204ac44c1192859900b01aef60849d2
samiullah51 <samii51728@gmail.com>
2023-02-11 23:33:18 +0500

    search functionality added

--- src/pages/Search/Search.js
+++ src/pages/Search/Search.js
@@ -1,11 +1,54 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router";
 import { useLocation } from "react-router-dom";
+import Navbar from "../../components/Navbar/Navbar";
+import SoldProduct from "../../components/SoldProducts/Product/Product";
+import { loader } from "../../loader";
+import { publicRequest } from "../../requestMethods";
+import "./Search.css";
 function Search() {
   const search = useLocation().search;
-  const cat = new URLSearchParams(search).get("product");
+  const product = new URLSearchParams(search).get("product");
+  const [searchProducts, setSearchProducts] = useState([]);
+  //   loading
+  const [loading, setLoading] = useState(false);
+  // fetch the search product
+  useEffect(() => {
+    const searchProducts = async () => {
+      setLoading(true);
+      try {
+        const searched = await publicRequest.get(
+          `/product/sell/find?search=${product}`
+        );
+        setSearchProducts(searched.data);
+        setLoading(false);
+        console.log(searched.data);
+      } catch (err) {
+        console.log(err.message);
+      }
+    };
+    searchProducts();
+  }, [product]);
 
-  return <div>Search {cat}</div>;
+  return (
+    <>
+      <Navbar />
+      <div className="search">
+        <h1>Search for "{product}"</h1>
+        {!loading ? (
+          <div className="searched__products">
+            {searchProducts.map((product) => (
+              <SoldProduct key={product._id} product={product} />
+            ))}
+          </div>
+        ) : (
+          <div className="searched__products">
+            <img src={loader} width={40} />
+          </div>
+        )}
+      </div>
+    </>
+  );
 }
 
 export default Search;
