2e3e67051bf9d7e9a137aab0e68d6b71c35d0bb2
samiullah51 <samii51728@gmail.com>
2023-02-17 00:52:34 +0500

    navbar added

--- src/components/Navbar/RightNavbar/RightNavbar.js
+++ src/components/Navbar/RightNavbar/RightNavbar.js
@@ -2,41 +2,50 @@ import React from "react";
 import { useState } from "react";
 import "./RightNavbar.css";
 import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import ExitToAppIcon from "@mui/icons-material/ExitToApp";
+import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
+import EqualizerIcon from "@mui/icons-material/Equalizer";
+import SwapHorizIcon from "@mui/icons-material/SwapHoriz";
+import TurnSlightRightIcon from "@mui/icons-material/TurnSlightRight";
+import TurnSlightLeftIcon from "@mui/icons-material/TurnSlightLeft";
+import PermIdentityIcon from "@mui/icons-material/PermIdentity";
+import ChatBubbleOutlineIcon from "@mui/icons-material/ChatBubbleOutline";
+import { Link } from "react-router-dom";
 function RightNavbar() {
   const [dropdown, setDropdown] = useState(false);
-  const [mode, setMode] = useState("SELLER");
+  const [mode, setMode] = useState("BUY");
   return (
     <div className="right__navbar">
-      <p className="label">Switch to</p>
-      <div className="switcher" onClick={() => setDropdown(!dropdown)}>
-        <div className="mode">
-          <p>{mode}</p>
-          <KeyboardArrowDownIcon />
-        </div>
-        {dropdown && (
-          <div className="show">
-            {mode === "SELLER" && (
-              <>
-                <p onClick={() => setMode("BUYER")}>BUYER</p>
-                <p onClick={() => setMode("EXCHANGER")}>EXCHANGER</p>
-              </>
-            )}
-            {mode === "BUYER" && (
-              <>
-                <p onClick={() => setMode("SELLER")}>SELLER</p>
-                <p onClick={() => setMode("EXCHANGER")}>EXCHANGER</p>
-              </>
-            )}
-            {mode === "EXCHANGER" && (
-              <>
-                <p onClick={() => setMode("BUYER")}>BUYER</p>
-                <p onClick={() => setMode("SELLER")}>SELLER</p>
-              </>
-            )}
-          </div>
-        )}
+      <Link to="/currentuserprofile">
+        <TurnSlightRightIcon />
+        <p>Sell</p>
+      </Link>
+      <Link to="/">
+        <TurnSlightLeftIcon />
+        <p>Buy</p>
+      </Link>
+      <Link to="/addexchangeproduct">
+        <SwapHorizIcon />
+        <p>Exchange</p>
+      </Link>
+      <Link>
+        <EqualizerIcon />
+        <p>Analytics</p>
+      </Link>
+      <Link>
+        <NotificationsNoneIcon />
+        <p>Notifications</p>
+      </Link>
+
+      <Link to="/chatbox">
+        <ChatBubbleOutlineIcon />
+        <p>Chats</p>
+      </Link>
+
+      <div className="label">
+        <p>LogOut </p>
+        <ExitToAppIcon />
       </div>
-      <p className="label">Mode</p>
     </div>
   );
 }
