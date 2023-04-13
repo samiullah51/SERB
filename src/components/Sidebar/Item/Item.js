200f7633f10d45f5a20d84ac4d4ab6e7e0a061c1
samiullah51 <samii51728@gmail.com>
2023-04-13 23:56:55 +0500

    working on award page

--- src/components/Sidebar/Item/Item.js
+++ src/components/Sidebar/Item/Item.js
@@ -1,7 +1,31 @@
 import React from "react";
 import "./Item.css";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+import { Link } from "react-router-dom";
+import EmojiEventsOutlinedIcon from "@mui/icons-material/EmojiEventsOutlined";
+import VisibilityOutlinedIcon from "@mui/icons-material/VisibilityOutlined";
+import AttachMoneyOutlinedIcon from "@mui/icons-material/AttachMoneyOutlined";
 function Item() {
-  return <div>Item</div>;
+  return (
+    <div className="items">
+      <Link className="single__item active">
+        <ShoppingCartOutlinedIcon />
+        <p>Sales</p>
+      </Link>
+      <Link className="single__item">
+        <EmojiEventsOutlinedIcon />
+        <p>Awards</p>
+      </Link>
+      <Link className="single__item">
+        <VisibilityOutlinedIcon />
+        <p>Views</p>
+      </Link>
+      <Link className="single__item">
+        <AttachMoneyOutlinedIcon />
+        <p>Revenue</p>
+      </Link>
+    </div>
+  );
 }
 
 export default Item;
--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,9 +1,10 @@
 import React from "react";
 import "./Sidebar.css";
+import Item from "./Item/Item";
 function Sidebar() {
   return (
     <div className="sidebar">
-      <p>sdf</p>
+      <Item />
     </div>
   );
 }
