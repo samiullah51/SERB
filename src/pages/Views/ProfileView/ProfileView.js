70d8f6fdf6b95dabea1099ef0cb26529078c0bc9
samiullah51 <samii51728@gmail.com>
2023-04-16 00:03:41 +0500

    nested routing

--- src/pages/Views/ProfileView/ProfileView.js
+++ src/pages/Views/ProfileView/ProfileView.js
@@ -4,6 +4,7 @@ import Navbar from "../../../components/Navbar/Navbar";
 import Sidebar from "../../../components/Sidebar/Sidebar";
 import ProfileStats from "./ProfileStats";
 import ProfileViews from "../../../components/Views/ProfileViews";
+import ProfileVieww from "../../../components/Views/ProfileView/ProfileView";
 function ProfileView() {
   return (
     <>
@@ -11,8 +12,19 @@ function ProfileView() {
       <div className="views">
         {/* Sidebar */}
         <Sidebar />
-        <ProfileView />
-        <ProfileStats />
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+            flexBasis: "80%",
+            padding: "20px",
+          }}
+        >
+          <ProfileVieww />
+          <ProfileStats />
+        </div>
       </div>
     </>
   );
--- src/pages/Views/TotalProductView/TotalProductView.js
+++ src/pages/Views/TotalProductView/TotalProductView.js
@@ -3,6 +3,8 @@ import "./../Views.css";
 import Navbar from "../../../components/Navbar/Navbar";
 import Sidebar from "../../../components/Sidebar/Sidebar";
 import ProfileStats from "./ProfileStats";
+import ProfileVieww from "../../../components/Views/ProfileView/ProfileView";
+
 function TotalProductView() {
   return (
     <>
@@ -10,7 +12,19 @@ function TotalProductView() {
       <div className="views">
         {/* Sidebar */}
         <Sidebar />
-        <ProfileStats />
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            flexBasis: "80%",
+            alignItems: "center",
+            justifyContent: "center",
+            padding: "20px",
+          }}
+        >
+          <ProfileVieww />
+          <ProfileStats />
+        </div>
       </div>
     </>
   );
