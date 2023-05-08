db1869e1b70da1cdac7af2f74f6d60364ea27525
samiullah51 <samii51728@gmail.com>
2023-05-08 18:31:09 +0100

    test

--- src/redux/User/userTypes.js
+++ src/redux/User/userTypes.js
@@ -2,3 +2,4 @@ export const LOG_IN = "LOG_IN";
 export const LOG_OUT = "LOG_OUT";
 export const SELECTED = "SELECTED";
 export const CURRENT_CHAT = "CURRENT_CHAT";
+export const CURRENT_CHAT = "CURRENT_CHAT";
