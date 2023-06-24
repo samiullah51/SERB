edc6de6572103ee56371277a268aa55fd1595083
samiullah51 <samii51728@gmail.com>
2023-06-24 12:39:31 +0500

    notifications structure done

--- src/pages/Notifications/Notifications.jsx
+++ src/pages/Notifications/Notifications.jsx
@@ -1,7 +1,39 @@
 import React from "react";
 import "./Notifications.css";
+import Navbar from "../../components/Navbar/Navbar";
 function Notifications() {
-  return <div>Notifications</div>;
+  return (
+    <>
+      <Navbar />
+      <div className="notifications">
+        <div className="notifi__left">
+          <div className="notifi__left__header">
+            <h2>Notifications</h2>
+          </div>
+          <div
+            className="line"
+            style={{ width: "100%", height: "1px", backgroundColor: "#fff" }}
+          ></div>
+          {/* notifications */}
+          <div className="notifications__list">
+            <div className="single__notification">single notifications</div>
+            <div className="single__notification">single notifications</div>
+          </div>
+        </div>
+        <div className="notifi__right">
+          <div className="notifi__right__header">
+            <div className="notifi__info">
+              <p>sdf</p>
+            </div>
+          </div>
+          <div
+            className="line"
+            style={{ width: "100%", height: "1px", backgroundColor: "gray" }}
+          ></div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Notifications;
